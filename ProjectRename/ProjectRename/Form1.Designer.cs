10843da50de6298cf068b313d09877b4adb580ea
chenwei2015 <50619671+chenwei2015@users.noreply.github.com>
2020-11-25 11:38:33 +0800

    Initial commit

---
+++ ProjectRename/ProjectRename/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace ProjectRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.operatorfolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.source = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dest = new System.Windows.Forms.TextBox();
+            this.RenameFile = new System.Windows.Forms.CheckBox();
+            this.renamect = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // operatorfolder
+            // 
+            this.operatorfolder.Location = new System.Drawing.Point(89, 13);
+            this.operatorfolder.Name = "operatorfolder";
+            this.operatorfolder.Size = new System.Drawing.Size(261, 21);
+            this.operatorfolder.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件夹路径";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(301, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "替换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "源字符";
+            // 
+            // source
+            // 
+            this.source.Location = new System.Drawing.Point(90, 81);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(81, 21);
+            this.source.TabIndex = 4;
+            this.source.Text = "_MTR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "      →        目标字符";
+            // 
+            // dest
+            // 
+            this.dest.Location = new System.Drawing.Point(332, 81);
+            this.dest.Name = "dest";
+            this.dest.Size = new System.Drawing.Size(81, 21);
+            this.dest.TabIndex = 7;
+            // 
+            // RenameFile
+            // 
+            this.RenameFile.AutoSize = true;
+            this.RenameFile.Checked = true;
+            this.RenameFile.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.RenameFile.Location = new System.Drawing.Point(90, 40);
+            this.RenameFile.Name = "RenameFile";
+            this.RenameFile.Size = new System.Drawing.Size(84, 16);
+            this.RenameFile.TabIndex = 8;
+            this.RenameFile.Text = "替换文件名";
+            this.RenameFile.UseVisualStyleBackColor = true;
+            // 
+            // renamect
+            // 
+            this.renamect.AutoSize = true;
+            this.renamect.Checked = true;
+            this.renamect.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.renamect.Location = new System.Drawing.Point(90, 62);
+            this.renamect.Name = "renamect";
+            this.renamect.Size = new System.Drawing.Size(96, 16);
+            this.renamect.TabIndex = 9;
+            this.renamect.Text = "替换文件内容";
+            this.renamect.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(90, 112);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(321, 196);
+            this.checkedListBox1.TabIndex = 10;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "文件类型选择";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(356, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(89, 322);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(48, 16);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "全选";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "替换方式";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 349);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.renamect);
+            this.Controls.Add(this.RenameFile);
+            this.Controls.Add(this.dest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.source);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.operatorfolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "重命名工具 - 1.00";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox operatorfolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox source;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dest;
+        private System.Windows.Forms.CheckBox RenameFile;
+        private System.Windows.Forms.CheckBox renamect;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ProjectRename/ProjectRename/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjectRename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> filetypes = new List<string>();
+
+        
+        void searchTypes(string path)
+        {
+            filetypes = new List<string>();
+            checkedListBox1.Items.Clear();
+            DirectoryInfo Dir = new DirectoryInfo(path);
+            {
+                foreach (FileInfo f in Dir.GetFiles("*", SearchOption.AllDirectories)) //查找文件
+                {
+                  if (!filetypes.Contains(f.Extension))
+                  {
+                      filetypes.Add(f.Extension);
+                      checkedListBox1.Items.Add(f.Extension);
+                      checkedListBox1.SetItemChecked(checkedListBox1.Items.Count-1,true);
+                      if (f.Extension.ToLower().Contains("xls")|| f.Extension.ToLower().Contains("pdf"))
+                      {
+                          checkedListBox1.SetItemChecked(checkedListBox1.Items.Count - 1, false);
+                      }
+                  }
+                }
+              
+            }
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                operatorfolder.Text = folderBrowserDialog1.SelectedPath;
+                searchTypes(operatorfolder.Text);
+            }
+        }
+        bool Rename(string name,string repls,string tor)
+        {
+            try
+            {
+                string name_org = name;
+                name = name.Replace(repls, tor);
+                FileInfo fi = new FileInfo(name_org);
+                fi.MoveTo(name);
+                replaced = true;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        bool RenameContent(string name, string repls, string tor)
+        {
+          //  try
+            {
+                string str = File.ReadAllText(name);
+                str = str.Replace(repls, tor);
+                File.WriteAllText(name, str);
+                replaced = true;
+             }
+            //catch(Exception e)
+            //{
+            //    MessageBox.Show(e.Message);
+            //    return false;
+            //}
+            return true;
+        }
+
+        bool replaced = false;
+        bool Replace(string path)
+        {
+            replaced = false;
+            DirectoryInfo Dir = new DirectoryInfo(path);          
+            {
+
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (checkedListBox1.GetItemChecked(i))
+                    {
+                        foreach (FileInfo f in Dir.GetFiles("*" + filetypes[i], SearchOption.AllDirectories)) //查找文件
+                        {
+                            if (renamect.Checked)
+                            {
+                                if (!RenameContent(f.FullName, source.Text, dest.Text))
+                                {
+                                    return false;
+                                }
+                            }
+                            if(RenameFile.Checked)
+                            {
+                                if (f.FullName.EndsWith(filetypes[i]))
+                                {
+                                    if(!Rename(f.FullName, source.Text, dest.Text) )
+                                    {
+                                        return false;
+                                    }
+                                    
+                                }
+                            }
+                            
+                           
+                        }
+                    }
+                }
+             
+               
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(operatorfolder.Text))
+            {
+                MessageBox.Show(string.Format("目录不存在:{0}，请确认！",operatorfolder.Text));
+                return;
+            }
+            if(Replace(operatorfolder.Text))
+                MessageBox.Show("替换成功");
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, checkBox3.Checked);
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            
+        }
+    }
+}
